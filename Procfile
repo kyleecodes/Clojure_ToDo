7cb547e6ab47faa5c2108ed9a2c03c80cf6adf92
kyleecodes <kfieldsinmd@gmail.com>
2020-07-19 00:00:57 -0400

    Added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/todoapp.jar clojure.main -m todoapp.core $PORT
--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.2.1"]
                  [compojure "1.1.6"]]
+  :min-lein-version "2.0.0"
+  :uberjar-name "todoapp.jar"
   :main todoapp.web
   :profiles {:dev
              {:main todoapp.web/-dev-main}})
